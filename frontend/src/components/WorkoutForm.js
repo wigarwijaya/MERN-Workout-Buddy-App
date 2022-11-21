4fc209a0cf1333eec0838b1d871ba3620e881a2d
wigarwijaya <wigarwijaya@gmail.com>
2022-11-21 15:44:12 +0700

    starter

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,76 +1,110 @@
-import { useState } from 'react'
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutForm = () => {
-  const { dispatch } = useWorkoutsContext()
+  const { dispatch } = useWorkoutsContext();
 
-  const [title, setTitle] = useState('')
-  const [load, setLoad] = useState('')
-  const [reps, setReps] = useState('')
-  const [error, setError] = useState(null)
-//   const [emptyFields, setEmptyFields] = useState([])
+  const [workout, setWorkout] = useState({
+    title: "",
+    load: "",
+    reps: "",
+  });
+  const [error, setError] = useState(null);
+  const [titleIsValid, setTitleIsValid] = useState(true);
+  const [loadIsValid, setLoadIsValid] = useState(true);
+  const [repsIsValid, setRepsIsValid] = useState(true);
+
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+
+    if (value.trim().length > 0) {
+      setTitleIsValid(true);
+      setLoadIsValid(true);
+      setRepsIsValid(true);
+    }
+
+    setWorkout((prevWorkout) => {
+      return {
+        ...prevWorkout,
+        [name]: value,
+      };
+    });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    const { title, load, reps } = workout;
 
-    const workout = {title, load, reps}
-    
-    const response = await fetch('/api/workouts', {
-      method: 'POST',
-      body: JSON.stringify(workout),
+    const work = { title, load, reps };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(work),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const json = await response.json()
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
     if (!response.ok) {
-      setError(json.error)
-    //   setEmptyFields(json.emptyFields)
+      setError(json.error);
     }
     if (response.ok) {
-    //   setEmptyFields([])
-      setError(null)
-      setTitle('')
-      setLoad('')
-      setReps('')
-      dispatch({type: 'CREATE_WORKOUT', payload: json})
+      setError(null);
+      setWorkout({
+        title: "",
+        load: "",
+        reps: "",
+      });
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
-
-  }
+    if (title.trim().length === 0) {
+      setTitleIsValid(false);
+    }
+    if (load.trim().length === 0) {
+      setLoadIsValid(false);
+    }
+    if (reps.trim().length === 0) {
+      setRepsIsValid(false);
+      return;
+    }
+  };
 
   return (
-    <form className="create" onSubmit={handleSubmit}> 
+    <form className="create" onSubmit={handleSubmit}>
       <h3>Add a New Workout</h3>
 
       <label>Excersize Title:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setTitle(e.target.value)} 
-        value={title}
-        className={!title ? 'error' : ''}
+      <input
+        type="text"
+        onChange={changeHandler}
+        name="title"
+        value={workout.title}
+        className={!titleIsValid ? "error" : undefined}
       />
 
       <label>Load (in kg):</label>
-      <input 
-        type="number" 
-        onChange={(e) => setLoad(e.target.value)} 
-        value={load}
-        className={!load ? 'error' : ''}
+      <input
+        type="number"
+        onChange={changeHandler}
+        name="load"
+        value={workout.load}
+        className={!loadIsValid ? "error" : undefined}
       />
 
       <label>Number of Reps:</label>
-      <input 
-        type="number" 
-        onChange={(e) => setReps(e.target.value)} 
-        value={reps}
-        className={!reps ? 'error' : ''}
+      <input
+        type="number"
+        onChange={changeHandler}
+        name="reps"
+        value={workout.reps}
+        className={!repsIsValid ? "error" : undefined}
       />
 
       <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
     </form>
-  )
-}
+  );
+};
 
-export default WorkoutForm
+export default WorkoutForm;
